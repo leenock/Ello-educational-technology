26c104f87dac59e3bee5d842462c9bc4694bea91
leenock <leenock176@gmail.com>
2024-06-09 16:44:17 +0300

    front end req. installs

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Button} from '@mui/material';
+
+function App() {
+  return (
+    <div>
+      <Button color="primary" variant="contained">
+        Click Me
+      </Button>
+    </div>
+  );
+}
+
+export default App;
