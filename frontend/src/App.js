1578fc0af501bb38160e688480d58cb55ab86606
leenock <leenock176@gmail.com>
2024-06-11 14:58:18 +0300

    ..

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,13 +13,14 @@ const App = () => {
   const [searchResults, setSearchResults] = useState([]);
   const [readingList, setReadingList] = useState([]);
 
+  // filtering books based on the search term
   useEffect(() => {
     const results = books.filter((book) =>
       book.title.toLowerCase().includes(searchTerm.toLowerCase())
     );
     setSearchResults(results);
   }, [searchTerm, books]);
-
+  // Function adding a book
   const handleAdd = (book) => {
     if (!readingList.some((item) => item.title === book.title)) {
       setReadingList([...readingList, book]);
@@ -27,7 +28,7 @@ const App = () => {
     setSearchTerm("");
     setSearchResults([]);
   };
-
+  // Function removing a book
   const handleRemove = (bookToRemove) => {
     setReadingList(readingList.filter((book) => book !== bookToRemove));
   };
@@ -38,17 +39,17 @@ const App = () => {
       <Container style={{ marginBottom: "150px" }}>
         <Header />
         <br />
-        <Typography variant="h5" gutterBottom style={{fontWeight: 'bold'}}>
+        <Typography variant="h5" gutterBottom style={{ fontWeight: "bold" }}>
           Search Book
         </Typography>
         <SearchBar
-          searchTerm={searchTerm}
-          setSearchTerm={setSearchTerm}
-          searchResults={searchResults}
-          handleAdd={handleAdd}
+          searchTerm={searchTerm} //  text input by the user in the search bar.
+          setSearchTerm={setSearchTerm} //  updating the search term as the user types in the search bar
+          searchResults={searchResults} //  array of search results that match the current search term
+          handleAdd={handleAdd} // add a selected book to the reading list & clear the search term and results.
         />
         <br />
-        <Typography variant="h5" gutterBottom style={{fontWeight: 'bold'}}>
+        <Typography variant="h5" gutterBottom style={{ fontWeight: "bold" }}>
           Explore Ello reading sets
         </Typography>
         <ReadingList books={readingList} onRemove={handleRemove} />
